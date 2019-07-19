55689badfd03b1ab4cfec147a51f5ce4db802849
Andre Teles <andre.telestp@gmail.com>
2019-07-19 06:15:18 -0300

    criado model de users

---
+++ src/app/models/User.js
@@ -0,0 +1,15 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      { sequelize }
+    );
+  }
+}
