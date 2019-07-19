d843eed61448527184f6687f611ea74e5b23451e
Andre Teles <andre.telestp@gmail.com>
2019-07-19 06:23:59 -0300

    criado loader de models da aplicacao

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'hello' });
+routes.get('/', async (req, res) => {
+  const response = await User.create({
+    name: 'dummy dummy',
+    email: 'dummy@dummy.com',
+    password_hash: '1213123123123',
+  });
+  return res.json(response);
 });
 
 export default routes;
