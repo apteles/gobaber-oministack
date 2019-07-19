661e60acbe488aa61617206ae25896d184f13291
Andre Teles <andre.telestp@gmail.com>
2019-07-19 07:03:47 -0300

    configurado session com jwt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -21,4 +21,8 @@ export default class User extends Model {
     });
     return this;
   }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 
