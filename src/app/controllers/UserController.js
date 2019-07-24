3e6255a8bf86ba36b85fe4ea628ac7dc21ec3927
Andre Teles <andre.telestp@gmail.com>
2019-07-24 07:07:21 -0300

    corrgido chamada de metodo errada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -37,7 +37,7 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      oldPassword: Yup.string()().min(6),
+      oldPassword: Yup.string().min(6),
       password: Yup.string()
         .min(6)
         .when('oldPassword', (oldPassword, field) =>
