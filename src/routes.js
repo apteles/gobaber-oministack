e787fdb02ec13395fd223109ef4874656e712031
Andre Teles <andre.telestp@gmail.com>
2019-07-26 05:49:04 -0300

    criado listagem de prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 import multerConfig from './config/multer';
-import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
