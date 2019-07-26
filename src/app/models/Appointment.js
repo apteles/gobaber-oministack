6338929ac6da18017f568bd08a452e9ff99f0feb
Andre Teles <andre.telestp@gmail.com>
2019-07-26 05:54:43 -0300

    criado model de agendamento

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.STRING,
+        canceled_at: Sequelize.DATE,
+      },
+      { sequelize }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
