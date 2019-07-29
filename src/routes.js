78e7d00fed894f9e82d3e7098c375d174a0d676d
Andre Teles <andre.telestp@gmail.com>
2019-07-29 06:50:34 -0300

    configurado rotinas de notifications e disparo de emails

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 import multerConfig from './config/multer';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,8 +27,11 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedules', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
